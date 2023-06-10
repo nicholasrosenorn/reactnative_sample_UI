5927ce6a4af2e6edfa0c79f55635654561da7ceb
nicholasrosenorn <nrosenor@purdue.edu>
2023-06-10 17:00:07 -0400

    search functionality

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -102,6 +102,7 @@ const JobDetails = () => {
                         )
                     }
             </ScrollView>
+            <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/result'} />
         </>
     </SafeAreaView>
   )
